393ef35c91f68fe04ad8a5a44ebbaee0ad572229
Leah Miller <leahmiller1969@gmail.com>
2019-02-07 09:54:56 -0800

    import VuetablePagination

--- src/components/MyVuetable.vue
+++ src/components/MyVuetable.vue
@@ -13,10 +13,12 @@
 import Vuetable from 'vuetable-2/src/components/Vuetable'
 import accounting from 'accounting'
 import moment from 'moment'
+import VuetablePagination from 'vuetable-2/src/components/VuetablePagination'
 
 export default {
   components: {
-    Vuetable
+    Vuetable,
+    VuetablePagination
   },
   data () {
     return {
