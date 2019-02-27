f2e1afbfd9856fe452f4ba77341cefdcfcb6bf16
Leah Miller <leahmiller1969@gmail.com>
2019-02-27 12:55:53 -0800

    clean up code

--- src/components/VuetableTwo.vue
+++ src/components/VuetableTwo.vue
@@ -65,30 +65,7 @@ export default {
       ],
       moreParams: {
 
-      },
-      // css: {
-      //   table: {
-      //     tableClass: 'table table-striped table-bordered table-hovered',
-      //     loadingClass: 'loading',
-      //     ascendingIcon: 'glyphicon glyphicon-chevron-up',
-      //     descendingIcon: 'glyphicon glyphicon-chevron-down',
-      //     handleIcon: 'glyphicon glyphicon-menu-hamburger',
-      //   },
-      //   pagination: {
-      //     infoClass: 'pull-left',
-      //     // wrapperClass: 'vuetable-pagination pull-right',
-      //     activeClass: 'btn-primary',
-      //     disabledClass: 'disabled',
-      //     pageClass: 'btn btn-border',
-      //     linkClass: 'btn btn-border',
-      //     icons: {
-      //       first: '',
-      //       prev: '',
-      //       next: '',
-      //       last: '',
-      //     },
-      //   }
-      // }
+      }
     }
   },
   mounted() {
